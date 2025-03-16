df9dc3c4669be5f1ab3de486925d1b78563d7e11
CallmeSerge <serg.kharlamoff@mail.ru>
2025-03-16 19:52:47 +0300

    NewCommit

--- SeaBattle/Assets/Scripts/ShipsManager.cs
+++ SeaBattle/Assets/Scripts/ShipsManager.cs
@@ -8,13 +8,17 @@ public class ShipsManager : MonoBehaviour
     [field: SerializeField] public List<GameObject> _shipPrefabs { get; private set; }
     private void Start()
     {
-        CreateShip();
+        StartCoroutine(CreateShip());
     }
 
-    public void CreateShip()
+    IEnumerator CreateShip()
     {
-        int selectedShip = Random.Range(0,_shipPrefabs.Count);
-        Instantiate(_shipPrefabs[selectedShip], _tegForShipAwake.transform.position, Quaternion.identity);
-        _shipPrefabs.RemoveAt(selectedShip);
+        while (_shipPrefabs.Count > 0)
+        {
+            int selectedShip = Random.Range(0, _shipPrefabs.Count);
+            Instantiate(_shipPrefabs[selectedShip], _tegForShipAwake.transform.position, Quaternion.identity);
+            _shipPrefabs.RemoveAt(selectedShip);
+            yield return new WaitForSeconds(5);
+        }
     }
 }
