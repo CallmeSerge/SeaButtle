ba8b656e609efd33df869e1df72bc4e64c012725
CallmeSerge <serg.kharlamoff@mail.ru>
2025-03-21 13:50:26 +0300

    newCommit 21.03.2025

--- newSeaBattle/Assets/Scripts/PeriskopMove.cs
+++ newSeaBattle/Assets/Scripts/PeriskopMove.cs
@@ -6,24 +6,25 @@ using UnityEngine;
 public class PeriskopMove : MonoBehaviour
 {
     [SerializeField] private float _periskopSpeed;
+    [SerializeField] private Rigidbody2D _rb;
     void Update()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left * _periskopSpeed * Time.deltaTime);
-            if (transform.position.x < -21)
-            {
-                transform.position = new Vector2(-21, transform.position.y);
-            }
+            _rb.AddForce(Vector2.left * _periskopSpeed);    
+        }
+        if (transform.position.x < -21)
+        {
+            transform.position = new Vector2(-21, transform.position.y);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.right * _periskopSpeed * Time.deltaTime);
-            if (transform.position.x > 21)
-                {
-                    transform.position = new Vector2(21, transform.position.y);
-                }
+            _rb.AddForce(Vector2.right * _periskopSpeed);
+        }
+        if (transform.position.x > 21)
+        {
+            transform.position = new Vector2(21, transform.position.y);
         }
     }
 }
